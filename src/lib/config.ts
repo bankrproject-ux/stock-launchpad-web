b4ae88149c9f4e4bd01150d753e7a9645c90a8bf
bankrproject-ux <bankrprojek@gmail.com>
2026-08-28 02:20:40 +0700

    Create config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,25 @@
+export const LAUNCHPAD_ADDRESS =
+  "0x3B95e23c5d00b31E76cDf689D50C7CC4755a668D" as const;
+
+export const ROBINHOOD_CHAIN_ID = 4663;
+
+export const PAIR_ASSETS = [
+  {
+    name: "WETH",
+    symbol: "WETH",
+    address:
+      "0x0Bd7D308f8E1639FAb988df18A8011f41EAcAD73" as const
+  },
+  {
+    name: "USDG",
+    symbol: "USDG",
+    address:
+      "0x5fc5360D0400a0Fd4f2af552ADD042D716F1d168" as const
+  },
+  {
+    name: "NVIDIA",
+    symbol: "NVDA",
+    address:
+      "0xd0601CE157Db5bdC3162BbaC2a2C8aF5320D9EEC" as const
+  }
+];
